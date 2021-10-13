9b293a1410b548d98a0788dd7e891de3be875c5a
larryyangsen <larryyangsen@hotmail.com>
2021-10-13 19:34:54 +0800

    [feat] use args

--- 50-projects-nextjs/components/expanding-card.stories.tsx
+++ 50-projects-nextjs/components/expanding-card.stories.tsx
@@ -6,20 +6,21 @@ export default {
     component: ExpandingCards,
 } as ComponentMeta<typeof ExpandingCards>;
 
-export const Cards = () => (
-    <ExpandingCards
-        width="500px"
-        cards={[
-            {
-                id: '1',
-                imageUrl: 'http://picsum.photos/600',
-                title: 'Card 1',
-            },
-            {
-                id: '2',
-                imageUrl: 'http://picsum.photos/500',
-                title: 'Card 2',
-            },
-        ]}
-    />
-);
+const Template: ComponentStory<typeof ExpandingCards> = (args) => <ExpandingCards {...args} />;
+export const Default = Template.bind({});
+
+Default.args = {
+    width: '500px',
+    cards: [
+        {
+            id: '1',
+            imageUrl: 'http://picsum.photos/600',
+            title: 'Card 1',
+        },
+        {
+            id: '2',
+            imageUrl: 'http://picsum.photos/500',
+            title: 'Card 2',
+        },
+    ],
+};
